45677b7f437c1b0082954f2bf6831dfa2c0f13e7
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-08-12 16:51:14 -0300

    Criação do método de menor valor e criação de testes

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -6,13 +6,18 @@ use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
-    private $maiorValor = 0;
+    private $maiorValor = -INF;
+    private $menorValor = INF;
 
     public function avalia(Leilao $leilao): void
     {
         foreach ($leilao->getLances() as $lance) {
             if($lance->getValor() > $this->maiorValor) {
                 $this->maiorValor = $lance->getValor();
+            } 
+            
+            if ($lance->getValor() < $this->menorValor) {
+                $this->menorValor = $lance->getValor();
             }
         }
     }
@@ -20,5 +25,10 @@ class Avaliador
     public function getMaiorValor(): float
     {
         return $this->maiorValor;
+    }
+
+    public function getMenorValor(): float
+    {
+        return $this->menorValor;
     }
 }
--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -52,4 +52,48 @@ class AvaliadorTest extends TestCase
         // Assert - Then
         self::assertEquals(2500, $maiorValor);
     }
+
+    public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemCrescente()
+    {
+        // Arrange - Given
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($maria, 2500));
+
+        $leiloeiro = new Avaliador();
+
+        // Act - When
+        $leiloeiro->avalia($leilao);
+
+        $menorValor = $leiloeiro->getMenorValor();
+
+        // Assert - Then
+        self::assertEquals(2000, $menorValor);
+    }
+
+    public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemDecrescente()
+    {
+        // Arrange - Given
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
+
+        $leilao->recebeLance(new Lance($joao, 2500));
+        $leilao->recebeLance(new Lance($maria, 2000));
+
+        $leiloeiro = new Avaliador();
+
+        // Act - When
+        $leiloeiro->avalia($leilao);
+
+        $menorValor = $leiloeiro->getMenorValor();
+
+        // Assert - Then
+        self::assertEquals(2000, $menorValor);
+    }
 }
