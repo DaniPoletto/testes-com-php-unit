6a18eedfb9292ef1a3bd47a2b35de38df693b2fe
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-08-12 17:29:10 -0300

    teste com nova função de retornar 3 maiores valores

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,12 +2,14 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
     private $maiorValor = -INF;
     private $menorValor = INF;
+    private $maioresLances;
 
     public function avalia(Leilao $leilao): void
     {
@@ -20,6 +22,13 @@ class Avaliador
                 $this->menorValor = $lance->getValor();
             }
         }
+
+        $lances = $leilao->getLances();
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();  
+        });
+
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
 
     public function getMaiorValor(): float
@@ -30,5 +39,10 @@ class Avaliador
     public function getMenorValor(): float
     {
         return $this->menorValor;
+    }
+
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -96,4 +96,28 @@ class AvaliadorTest extends TestCase
         // Assert - Then
         self::assertEquals(2000, $menorValor);
     }
+
+    public function testAvaliadorDeveBuscarOs3MaioresValores()
+    {
+        $leilao = new Leilao('Fiat');
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+        $ana = new Usuario('Ana');
+        $jorge = new Usuario('Jorge');
+
+        $leilao->recebeLance(new Lance($ana, 1500));
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 2000));
+        $leilao->recebeLance(new Lance($jorge, 1700));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+
+        $maiores = $leiloeiro->getMaioresLances();
+
+        self::assertCount(3, $maiores);
+        self::assertEquals(2000, $maiores[0]->getValor());
+        self::assertEquals(1700, $maiores[1]->getValor());
+        self::assertEquals(1500, $maiores[2]->getValor());
+    }
 }
