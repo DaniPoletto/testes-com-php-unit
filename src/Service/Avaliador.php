06a37a426ae316510695d8096334fda8d026c1ec
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-08-12 16:41:34 -0300

    correção do método avalia do Avaliador após testes

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -6,13 +6,15 @@ use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
-    private $maiorValor;
+    private $maiorValor = 0;
 
     public function avalia(Leilao $leilao): void
     {
-        $lances = $leilao->getLances();
-        $ultimoLance = $lances[count($lances) - 1];
-        $this->maiorValor = $ultimoLance->getValor();
+        foreach ($leilao->getLances() as $lance) {
+            if($lance->getValor() > $this->maiorValor) {
+                $this->maiorValor = $lance->getValor();
+            }
+        }
     }
 
     public function getMaiorValor(): float
---
+++ tests/Service/AvaliadorTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace Alura\Leilao\Tests\Service;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use PHPUnit\Framework\TestCase;
+use Alura\Leilao\Service\Avaliador;
+
+class AvaliadorTest extends TestCase
+{
+    public function testAvaliadorDeveEncontrarOMaiorValorDeLancesEmOrdemCrescente()
+    {
+        // Arrange - Given
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($maria, 2500));
+
+        $leiloeiro = new Avaliador();
+
+        // Act - When
+        $leiloeiro->avalia($leilao);
+
+        $maiorValor = $leiloeiro->getMaiorValor();
+
+        // Assert - Then
+        self::assertEquals(2500, $maiorValor);
+    }
+
+    public function testAvaliadorDeveEncontrarOMaiorValorDeLancesEmOrdemDecrescente()
+    {
+        // Arrange - Given
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('João');
+
+        $leilao->recebeLance(new Lance($joao, 2500));
+        $leilao->recebeLance(new Lance($maria, 2000));
+
+        $leiloeiro = new Avaliador();
+
+        // Act - When
+        $leiloeiro->avalia($leilao);
+
+        $maiorValor = $leiloeiro->getMaiorValor();
+
+        // Assert - Then
+        self::assertEquals(2500, $maiorValor);
+    }
+}
