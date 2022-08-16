719e3f57b7102d2379c505983e328a695461c4ff
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-08-15 21:54:55 -0300

    Implementação e testes para não permitir lance consecutivo do mesmo usuário

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -17,6 +17,11 @@ class Leilao
 
     public function recebeLance(Lance $lance)
     {
+
+        if (!empty($this->lances) && $this->ehDoUltimoUsuario($lance)) {
+            return;
+        }
+
         $this->lances[] = $lance;
     }
 
@@ -26,5 +31,11 @@ class Leilao
     public function getLances(): array
     {
         return $this->lances;
+    }
+
+    private function ehDoUltimoUsuario(Lance $lance) : bool
+    {
+        $ultimoLance = $this->lances[count($this->lances) - 1];
+        return $lance->getUsuario() == $ultimoLance->getUsuario();
     }
 }
--- tests/Model/LeilaoTest.php
+++ tests/Model/LeilaoTest.php
@@ -8,6 +8,18 @@ use PHPUnit\Framework\TestCase;
 
 class LeilaoTest extends TestCase
 {
+    public function testLeilaoNaoDeveReceberLancesRepetidos()
+    {
+        $leilao = new Leilao('Variant');
+        $ana = new Usuario('Ana');
+
+        $leilao->recebeLance(new Lance($ana, 1000));
+        $leilao->recebeLance(new Lance($ana, 1500));
+
+        static::assertCount(1, $leilao->getLances());
+        static::assertEquals(1000, $leilao->getLances()[0]->getValor());
+    }
+
     /**
      * @dataProvider geraLances
      */
